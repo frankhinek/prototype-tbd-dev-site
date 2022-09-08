8c052bb235ac9401d33883854f1b6e7ed7c4e7f3
Frank Hinek <frankhinek@users.noreply.github.com>
2022-09-08 16:32:31 -0400

    Add keys to BlankLines function.
    Per https://reactjs.org/docs/lists-and-keys.html#keys
    
    "Keys help React identify which items have changed, are added, or
    are removed. Keys should be given to the elements inside the
    array to give the elements a stable identity."
    
    Signed-off-by: Frank Hinek <frankhinek@users.noreply.github.com>

--- src/components/BlankLines.js
+++ src/components/BlankLines.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function BlankLines({lineCount}) {
   const result = [];
   for (let i = 0; i < lineCount; i++) {
-    result.push(<br />)
+    result.push(<br key={i}/>)
   }
   return (
     <p>{result}</p>
