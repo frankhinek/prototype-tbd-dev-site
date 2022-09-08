3e04cd9856f445e1fd5349eb4adb0c58888f8c2a
Frank Hinek <frankhinek@users.noreply.github.com>
2022-09-08 16:31:53 -0400

    fix: correct usage of className in React elements
    Signed-off-by: Frank Hinek <frankhinek@users.noreply.github.com>

--- src/pages/open-source.js
+++ src/pages/open-source.js
@@ -6,9 +6,9 @@ import BlankLines from '@site/src/components/BlankLines';
 function OpenSourceContent() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <div class="row mdxPageWrapper_yV3F">
-      <div class="col">
-        <div class="prose prose-pink">
+    <div className="row mdxPageWrapper_yV3F">
+      <div className="col">
+        <div className="prose prose-pink">
           <h1>
             Today’s financial systems leave people behind. We build systems for anyone with internet access.
             And we’re creating it like the web itself: as a public good.
@@ -27,7 +27,7 @@ export default function Home() {
       title="Open Source"
       description="Description will go into a meta tag in <head />">
       <main
-        class="container--fluid">
+        className="container--fluid">
         <OpenSourceContent />
       </main>
     </Layout>
